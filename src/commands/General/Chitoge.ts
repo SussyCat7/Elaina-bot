a77f21e0548143f09d838990ea9ca96ff73b6593
Aryastark2 <101760949+Aryastark2@users.noreply.github.com>
2022-04-03 01:18:21 +0530

    Update Chitoge.ts

--- src/commands/General/Chitoge.ts
+++ src/commands/General/Chitoge.ts
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `💕 *akuma* 💕\n\n🍀 *Description: A WhatsApp Bot developed by *sumith* With Rich Anime features based on akuma.*\n\n🌐 *OFFICIAL BOT URL:https://github.com/Ronen6999/Elaina-bot* \n\n 📒 *Guide:https://github.com/Ronen6999/Elaina-bot* \n\n 👾 *BOT URL:https://github.com/Ronen6999/Elaina-bot* \n`,
+				caption: `💕 *akuma* 💕\n\n🍀 *Description: A WhatsApp Bot developed by *sumith* With Rich Anime features based on akuma.*\n\n🌐 *OFFICIAL BOT URL:https://github.com/Aryastark2/Elaina-bot.git* \n\n 📒 *Guide:https://github.com/Aryastark2/Elaina-bot.git* \n\n 👾 *BOT URL:https://github.com/Aryastark2/Elaina-bot.git* \n`,
 			}
 		);
 	};
